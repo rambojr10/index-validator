8da1c72676d351e437c67f9336060f1e7aa91c71
Andres Valencia <valenciapipe1@gmail.com>
2024-06-12 08:44:15 -0500

    Back-up commit

--- src/configs.js
+++ src/configs.js
@@ -1,9 +1,9 @@
-const defaultConfigs = {
-    PORT: 5173,
-    API_KEY: '9b7ed72f2f0d86d0',
-    OPEN_AI_API_KEY: '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',
-    email: 'andres.valencia@talent.com',
-    password: 'Valencia10.',
-}
+// const defaultConfigs = {
+//     PORT: 5173,
+//     API_KEY: '9b7ed72f2f0d86d0',
+//     OPEN_AI_API_KEY: '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',
+//     email: 'andres.valencia@talent.com',
+//     password: 'Valencia10.',
+// }
 
-export default defaultConfigs
+// export default defaultConfigs
